b1f2c1b27aaa8e104a1a583ace1866f00bbdba3d
Brandon Smith <mail@brandonsmith.ninja>
2021-11-23 00:25:48 -0600

    Allow passing an expression to the CLI as an argument

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,20 @@ mod utils;
 #[macro_use]
 mod macros;
 
-use rust_lisp::start_repl;
+use std::{cell::RefCell, rc::Rc};
+
+use rust_lisp::{default_env, eval_block, parse, start_repl};
 
 fn main() {
-    start_repl(None);
+    match std::env::args().nth(1) {
+        Some(code) => {
+            let env_rc = Rc::new(RefCell::new(default_env()));
+
+            println!(
+                "{}",
+                eval_block(env_rc.clone(), parse(&code).filter_map(|a| a.ok().clone())).unwrap()
+            );
+        }
+        None => start_repl(None),
+    }
 }
