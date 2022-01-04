21577c0f4f3e9de0d52960fe07f181e6cd7f0ea3
Алекс Мозаидзе <alexman2622@gmail.com>
2022-01-05 00:47:06 +0400

    Handled unclosed quotes

--- src/parser.rs
+++ src/parser.rs
@@ -69,13 +69,14 @@ fn tokenize(code: &str) -> impl Iterator<Item = &str> {
 
         // strings
         if ch == '"' {
-            let match_end = match_pred(&code[index + 1..], |c| c != '"');
+            if let Some(contents_end_index) = code[index + 1..].find(|c| c == '"') {
+                    let string_end_index = index + contents_end_index + 2;
 
-            if let Some(contents_end_index) = match_end {
-                let string_end_index = index + contents_end_index + 3;
-
-                skip_to = Some(string_end_index);
-                return Some(&code[index..string_end_index]);
+                    skip_to = Some(string_end_index);
+                    return Some(&code[index..string_end_index]);
+            } else {
+                    skip_to = Some(code.len());
+                    return None;
             }
         }
 
@@ -372,9 +373,14 @@ fn read_atom(token: &str) -> Value {
         return Value::Float(as_float);
     }
 
-    if token.chars().next().map_or(false, |c| c == '"')
-        && token.chars().nth_back(0).map_or(false, |c| c == '"')
-    {
+    if token.chars().next().map_or(false, |c| c == '"') {
+        // Empty string edge-case
+        if token.chars().nth(1).map_or(false, |c| c == '"') {
+            return Value::String(String::from(""));
+        }
+
+        // TODO: Implement escaped characters
+
         return Value::String(String::from(&token[1..token.chars().count() - 1]));
     }
 
